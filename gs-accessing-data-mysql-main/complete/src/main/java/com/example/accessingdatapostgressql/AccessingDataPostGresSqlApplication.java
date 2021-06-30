9e647087b3380484f0d5f50a4235aafa87d9519d
dhan1978 <dhancloud1978@gmail.com>
2021-06-30 15:11:06 +0530

    fist commit

---
+++ gs-accessing-data-mysql-main/complete/src/main/java/com/example/accessingdatapostgressql/AccessingDataPostGresSqlApplication.java
@@ -0,0 +1,12 @@
+package com.example.accessingdatapostgressql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataPostGresSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessingDataPostGresSqlApplication.class, args);
+	}
+}
